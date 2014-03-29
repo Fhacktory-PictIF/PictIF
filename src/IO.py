a338ecf1ab60dd6854740f79fa351e65323996c1
Vongo <adelmarre@vongo.eu>
2014-03-29 13:49:49 +0100

    Clean repo Added I and O classes in IO.py

---
+++ src/IO.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class I():
+	"""Input mechanism"""
+	def __init__(self, arg):
+		self.arg = arg
+		
+
+class O():
+	"""Output mechanism"""
+	def __init__(self, arg):
+		self.arg = arg
+		
---
+++ src/Reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from IO import O as O
+
+
+class Reader(Component):
+	"""Allows to read a file / folder"""
+
+	def __init__(self, arg):
+		Component.__init__(self, arg)
+		self.arg = arg
+
+	def function():
+		pass
+		
