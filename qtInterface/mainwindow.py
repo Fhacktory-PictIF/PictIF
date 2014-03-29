884ede1aa44853ef9f2925f8e982f3b1c215390c
Rémi Domingues <remi.domingues@insa-lyon.fr>
2014-03-29 15:52:56 +0100

    First attempt of drag and drop in QT

--- qtInterface/mainwindow.py
+++ qtInterface/mainwindow.py
@@ -9,7 +9,105 @@
 import sys
 from PySide import QtCore, QtGui
 
+class UIBlock(QtGui.QGraphicsObject):
+
+    def __init__(self, filepath):
+        super(UIBlock, self).__init__()
+        #self.pixmap = QtGui.QPixmap.grabWidget(filepath)
+        pixmap = QtGui.QPixmap(filepath)
+
+        # below makes the pixmap half transparent
+        painter = QtGui.QPainter(pixmap)
+        painter.setCompositionMode(painter.CompositionMode_DestinationIn)
+        painter.fillRect(pixmap.rect(), QtGui.QColor(0, 0, 0, 127))
+        painter.end()
+
+    def dragEnterEvent(self, e):
+        print('drag enter event')
+        e.accept()
+
+    def dragLeageEvent(self, e):
+        print('drag lave event')
+        e.accept()
+
+    def dropEvent(self, e):
+        print('drop event')
+        e.accept()
+
+
+    def mouseMoveEvent(self, e):
+        print "mouse moved event"
+        if e.buttons() != QtCore.Qt.LeftButton:
+            return
+
+        # write the relative cursor position to mime data
+        mimeData = QtCore.QMimeData()
+        #mimeData.setText('%d,%d' % (e.x(), e.y()))
+
+        # make a QDrag
+        drag = QtGui.QDrag(self)
+        # put our MimeData
+        drag.setMimeData(mimeData)
+        # set its Pixmap
+        drag.setPixmap(self.pixmap)
+        # shift the Pixmap so that it coincides with the cursor position
+        drag.setHotSpot(e.pos())
+
+        # start the drag operation
+        # exec_ will return the accepted action from dropEvent
+        if drag.exec_(QtCore.Qt.CopyAction | QtCore.Qt.MoveAction) == QtCore.Qt.MoveAction:
+            print 'moved'
+        else:
+            print 'copied'
+
+
+    def mousePressEvent(self, e):
+        QtGui.QPushButton.mousePressEvent(self, e)
+        if e.button() == QtCore.Qt.LeftButton:
+            print 'Mouse pressed button'
+
+class UIParseBlock(UIBlock):
+    def __init__(self):
+        super(UIParseBlock, self).__init__("../img/upload.png")
+
+
+class BlocksGraphicsView(QtGui.QGraphicsView):
+    def __init__(self, parent=None):
+        super(BlocksGraphicsView, self).__init__(parent)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        print('graphics view drag enter')
+        e.accept()
+
+
+    def dropEvent(self, e):
+        # get the relative position from the mime data
+        print('graphics view drop')
+        mime = e.mimeData().text()
+        x, y = map(int, mime.split(','))
+        e.source().move(e.pos()-QtCore.QPoint(x, y))
+        # set the drop action as Move
+        e.setDropAction(QtCore.Qt.MoveAction)
+        #event.acceptProposedAction()
+        e.accept()
+
+    def mousePressEvent(self, ev):
+        print "mouse pressed 2"
+        if ev.button() == QtCore.Qt.LeftButton:
+            pass #Useful to create links
+
+
 class Ui_MainWindow(QtGui.QMainWindow):
+    blockScene = None
+    blocks = []
+
+    def addBlockPresentation(self, block):
+        self.blocks.append(block)
+        #self.blockScene.addPixmap(block)
+        self.blockScene.addItem(block)
+
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -18,9 +116,14 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.listeModule = QtGui.QListView(self.centralwidget)
         self.listeModule.setGeometry(QtCore.QRect(0, 220, 231, 341))
         self.listeModule.setObjectName("listeModule")
-        self.graphicsView = QtGui.QGraphicsView(self.centralwidget)
+        self.graphicsView = BlocksGraphicsView(self.centralwidget)
         self.graphicsView.setGeometry(QtCore.QRect(240, 90, 551, 461))
         self.graphicsView.setObjectName("graphicsView")
+
+        self.blockScene = QtGui.QGraphicsScene()
+        self.blockScene.setSceneRect(self.graphicsView.rect())
+        self.graphicsView.setScene(self.blockScene)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 27))
@@ -46,7 +149,6 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.fileMenu = self.menubar.addMenu('&File')
         self.fileMenu.addAction(exitAction)
 
-
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
         self.toolBar.setWindowTitle(QtGui.QApplication.translate("MainWindow", "toolBar", None, QtGui.QApplication.UnicodeUTF8))
@@ -57,12 +159,10 @@ class ControlMainWindow(QtGui.QMainWindow):
     self.ui =  Ui_MainWindow()
     self.ui.setupUi(self)
     self.ui.init_user()
-   
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     mySW = ControlMainWindow()
-
-
-
+    #mySW.ui.addBlockPresentation(UIParseBlock())
     mySW.show()
     sys.exit(app.exec_())
