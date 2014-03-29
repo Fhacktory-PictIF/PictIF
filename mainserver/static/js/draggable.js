7fc7908a99329e86f5a0cdd462a56dd71c00a735
golgoth <adrien.duffy@gmail.com>
2014-03-29 23:24:38 +0100

    new containers

--- mainserver/static/js/draggable.js
+++ mainserver/static/js/draggable.js
@@ -1,17 +1,7 @@
 $(document).ready(function() {
-        // on definit un point d'attache sur chaque conteneur
-    var e0 = jsPlumb.addEndpoint("container0"),
-    e1 = jsPlumb.addEndpoint("container1");
-        // puis on relie les deux points d'attache
-    jsPlumb.connect({ source:e0, target:e1 });
- 
-        // on rend le conteneur draggable mais seul les points d'attaches bougent, les deux divs restent a leur place
-       jsPlumb.draggable($("#container1"));
- 
- 
-        // sans passer par jsplumb, l'element est deplacable
-        $('#container3').draggable();
- 
+
+    var map = {};    
+
 
   var i = 0;
   var endpointStyle = {
@@ -22,40 +12,55 @@ $(document).ready(function() {
     };
 
 
+    var dropOptions = { hoverClass:"hover", activeClass:"active" },
+        exampleGreyEndpointOptions = {
+        endpoint:"Rectangle",
+        paintStyle:{ width:25, height:21, fillStyle:'#666' },
+        isSource:true,
+        connectorStyle : { strokeStyle:"#666" },
+        isTarget:true
+    };
+
   $('#container').dblclick(function(e) {
     var newState = $('<div>').attr('id', 'state' + i).addClass('itemDrag');
     
     var title = $('<div>').addClass('title').text('State ' + i);
     var connect = $('<div>').addClass('connect');
-    var receiver = $('<div>').addClass('receive');
-    
+
     newState.css({
-      'top': e.pageY,
-      'left': e.pageX
+      'top': e.layerX,  
+      'left': e.layerY
     });
-    
-    jsPlumb.makeTarget(receiver, {
-        parent: newState,
-      endpoint:{
-                                        anchor:[ "BottomRight", "BottomLeft"],
-                                        paintStyle:endpointStyle
-                                },
+    /*
+    jsPlumb.makeTarget(newState, {
+        parent: parent
 
     });
-
-
+*/
+/*
     jsPlumb.makeSource(connect, {
       parent: newState,
-      anchor: "BottomLeft"
+      anchor: "TopLeft"
     });
+*/
 
-    newState.append(connect);
+
     newState.append(title);
     
-    newState.append(receiver);
     
     $('#container').append(newState);
    jsPlumb.draggable($(".itemDrag"));
+
+    jsPlumb.addEndpoint('state'+i, {anchor:"Right", isSource:true, maxConnections:5, connectorStyle : { strokeStyle:"#666" }, endpoint:"Rectangle"});
+    jsPlumb.addEndpoint('state'+i,  {
+        anchor:"BottomLeft",
+        isTarget:true,
+        beforeDrop: function(params) {
+            return confirm("Connect " + params.sourceId + " to " + params.targetId + "?");
+        }}
+    );
+
+    jsPlumb.addEndpoint('state'+i,  {anchor:"TopLeft",isTarget:true});
     
     i++;    
   }); 
