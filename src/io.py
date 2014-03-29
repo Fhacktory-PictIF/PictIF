4f04065d116352d3ab209ed266d5a09d6e7bdb3f
Vongo <adelmarre@vongo.eu>
2014-03-29 15:52:25 +0100

    osef

--- src/io.py
+++ src/io.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from SimpleCV import Image
 import os
+from SimpleCV import Image
 
 class I():
 	"""Input mechanism"""
 	def __init__(self):
 		pass
 
-	def read(self, oseflol):
+	def read(self, path):
+		# print "Reading ", path
 		images = []
-		print "Reading ", oseflol
-		for dirname, dirnames, filenames in os.walk(oseflol):
-			for subdirname in dirnames:
-				print "Considering directory", subdirname
-				images.append(self.read(subdirname))
-			for filename in filenames:
-				print "Considering file ", filename
-				img = Image(dirname+filename)
-				img.show()
-				images.append(img)
+		if os.path.isfile(path):
+			# print "Considering file ", path
+			images.append(Image(path))
+		elif os.path.isdir(path):
+			# print "Considering directory ", path
+			for dirname, dirnames, filenames in os.walk(path):
+				for subdirname in dirnames:
+					# print "Considering directory", subdirname
+					images.append(self.read(subdirname))
+				for filename in filenames:
+					# print "Considering file ", filename
+					img = Image(dirname+filename)
+					img.show()
+					images.append(img)
 		return images
 
 class O():
 	"""Output mechanism"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self):
+		pass
+
+	def write(self, lol):
+		pass
 
 class ImageData():
 	"""Image object"""
