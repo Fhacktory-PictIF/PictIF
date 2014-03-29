f84f1ffcfb9674cdb4a8842de585e26aa13c6be1
vzantedeschi <vzantedeschi@gmail.com>
2014-03-29 21:55:54 +0100

    grayscale and green screen

--- src/component.py
+++ src/component.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 class Component():
 
     def __init__(self, name) :
@@ -13,15 +12,6 @@ class Component():
     def showOutput():
     	print 'print treated images'
 
-def createComponent(names):
-	comp = Component(names[0])
-	return names[1:], comp
-
 if __name__ == "__main__" :
 
-	import string
-	names = string.letters + string.digits
-
-	names, component = createComponent(names)
-	#cropper = Cropper(names[0],100,100,50,50)
-	#cropper.process()
+	pass
--- src/processors.py
+++ src/processors.py
@@ -2,44 +2,75 @@
 # -*- coding: utf-8 -*-
 
 from component import Component
-from IO import I, O
+import numpy
+from IO import I, O, ImageData
+from SimpleCV import Color
 
 class Cropper(Component):
-
-	def __init__(self, name, x, y, width, height):
-		Component.__init__(self, name)
-		self.input = I()
-		self.output = O()
-		self.x = x
-		self.y = y
-		self.width = width
-		self.height = height
-
-	def process(self):
-
-		img_input = self.input.readC(["../test/"])
-
-		images = [i.image.crop(self.x,self.y,self.width,self.height) for i in img_input]
-		self.executed = True
-		#TODO : replace with O.write()
-		for i,k in zip(images,range(4)):
-			i.save("../test/temp/imgT" + str(k) + ".jpeg")
-			img_input[k].unload()
-
-class Binarizer(Component):
 
 	def __init__(self, name):
 		Component.__init__(self, name)
 		self.input = I()
 		self.output = O()
+		self.x = 0
+		self.y = 0
+		self.width = 100
+		self.height = 100
 
 	def process(self):
-		#TODO : replace with I.read()
-		img_input = self.input.read("../test/")
 
-		images = [i.image.binarize() for i in img_input]
+		img_input = self.input.readC(["../test/"])
+
+		for im in img_input:
+			im.image = im.image.crop(self.x,self.y,self.width,self.height)
 		self.executed = True
-		#TODO : replace with O.write()
-		for i,k in zip(images,range(4)):
-			i.save("../test/imgT" + str(k) + ".jpeg")
-			img_input[k].unload()
+		
+		self.output.write(img_input,'../test/cropped/',self.name)
+
+class GrayScale(Component):
+
+	def __init__(self, name):
+		Component.__init__(self, name)
+		self.input = I()
+		self.output = O()
+		self.degree = 1
+
+	def process(self):
+		img_input = self.input.readC(["../test/"])
+
+		for im in img_input:
+			(red, green, blue) = im.image.splitChannels(False)
+			im.image = (red.toGray() + green.toGray() + blue.toGray()) / self.degree
+
+		self.executed = True
+		
+		self.output.write(img_input,'../binarized/',self.name)
+
+class ChromaKey(Component):
+
+	def __init__(self, name):
+		Component.__init__(self, name)
+		self.green_screen = ImageData("../test/greenScreen.jpg")
+		self.green_screen.load()
+		self.background = ImageData("../test/landscape.jpg")
+		self.background.load()
+		self.green_screen.image = self.green_screen.image.scale(self.background.image.width, self.background.image.height)
+
+	def process(self):
+		mask = self.green_screen.image.hueDistance(color=Color.GREEN).binarize()
+		import time
+		
+		result = (self.green_screen.image - mask) + (self.background.image - mask.invert())
+		result.show()
+		time.sleep(10)
+
+if __name__ == "__main__" :
+
+	import string
+	suffixes = string.letters + string.digits
+	binarizer = GrayScale(suffixes[0])
+	suffixes = suffixes[1:]
+	binarizer.process()
+	chroma = ChromaKey(suffixes[0])
+	suffixes = suffixes[1:]
+	chroma.process()
