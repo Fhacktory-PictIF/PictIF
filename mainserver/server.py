bf43aa96aca72c48c8030a0ec618f0ad26e8bdbb
golgoth <adrien.duffy@gmail.com>
2014-03-30 00:25:22 +0100

    server should not work

--- mainserver/server.py
+++ mainserver/server.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, Blueprint, current_app
 import json
 
 from core.component import Component
 
 app = Flask(__name__)
 app.debug = True
+
+#contient les component ajoutés, liés ou non à d'autres
+class ComponentGestion(object) :
+    def __init__(self):
+        self.map_of_component = {}
+        self.iteratorId = 0
+
+componentGestioner = ComponentGestion()
+
 
 @app.route("/")
 def index():
@@ -37,6 +46,31 @@ def getBlockFromType(blockId):
 def saveWorkFlow():
     resp = dict(ok=True)
     return json.dumps(resp)
+
+@app.route("/addConnection", methods = ['POST'])
+def addConnection() :
+    resp={'ok':True, 'result':''}
+    return json.dump(resp)
+
+@app.route("/removeConnection", methods = ['POST'])
+def removeConnection() :
+    pass
+
+@app.route("/addComponent", methods = ['POST'])
+def addComponent() :
+    if request.method == 'POST' :
+        id = componentGestioner.iteratorId+1
+        componentGestioner.iteratorId = componentGestioner.iteratorId+1
+        componentGestioner
+        resp = {'ok': True, 'result':id}
+        return json.dump(resp)
+    resp = {'ok': False, 'result':-1}
+    return json.dump(resp)
+
+
+@app.route("/removeComponent", methods = ['POST'])
+def removeComponent() :
+    pass
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
