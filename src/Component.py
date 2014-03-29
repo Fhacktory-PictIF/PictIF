24bbc2224dbb47b967dfba41de278faf9d3ab437
vzantedeschi <vzantedeschi@gmail.com>
2014-03-29 14:31:47 +0100

    adds Croper class

--- src/Component.py
+++ src/Component.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from IO import I, O
 
 class Component():
 
     def __init__(self, name) :
     	self.name = name
+
+    def process():
+    	print 'Abstract class'
+
+    def showOutput():
+    	print 'print treated images'
+
+class Croper(Component):
+
+	def __init__(self, name):
+		Component.__init__(self, name)
+		self.input = I("entry")
+		self.output = O("output")
+
+	def process():
+		images = [i.crop(100,100,50,50)]
+		return ''
 
 def createComponent(names):
 	comp = Component(names[0])
