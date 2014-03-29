b26af2e7378fb679cb44aa38d3048ed1ba1043ce
vzantedeschi <vzantedeschi@gmail.com>
2014-03-29 17:04:39 +0100

    adds Binarizer and Reader classes

--- src/component.py
+++ src/component.py
@@ -15,6 +15,25 @@ class Component():
     def showOutput():
     	print 'print treated images'
 
+class Reader(Component):
+
+	def __init__(self, name):
+		Component.__init__(self, name)
+		self.input = I()
+		self.output = O()
+		self.length = None
+		self.key_points = []
+		self.mean_colors = []
+
+	def process(self):
+		#TODO : replace with I.read()
+		img_input = self.input.read("../test/")
+
+		self.length = len(img_input)
+		self.key_points = [i.image.findKeypoints() for i in img_input]
+		self.mean_colors = [k.meanColor() for k in self.key_points]
+		self.executed = True
+
 class Cropper(Component):
 
 	def __init__(self, name, x, y, width, height):
@@ -28,13 +47,27 @@ class Cropper(Component):
 
 	def process(self):
 		#TODO : replace with I.read()
-		img_input = []
-		for i in range(1,5):
-			img = ImageData("../test/img" + str(i) + ".jpeg")
-			img.load()
-			img_input.append(img)
+		img_input = self.input.read("../test/")
 
 		images = [i.image.crop(self.x,self.y,self.width,self.height) for i in img_input]
+		self.executed = True
+		#TODO : replace with O.write()
+		for i,k in zip(images,range(4)):
+			i.save("../test/imgT" + str(k) + ".jpeg")
+			img_input[k].unload()
+
+class Binarizer(Component):
+
+	def __init__(self, name):
+		Component.__init__(self, name)
+		self.input = I()
+		self.output = O()
+
+	def process(self):
+		#TODO : replace with I.read()
+		img_input = self.input.read("../test/")
+
+		images = [i.image.binarize() for i in img_input]
 		self.executed = True
 		#TODO : replace with O.write()
 		for i,k in zip(images,range(4)):
